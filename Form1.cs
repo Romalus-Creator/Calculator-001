53db142a3c1f50a4fbd0cdb0036f4774af62e62c
Romalus-Creator <romanehrhardt101@gmail.com>
2022-09-11 16:50:37 -0500

    Got the Equal button finished, switch double variables to decimal variables so that the "decimal btn" can actually be used.

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,15 @@
+using System.Configuration;
 using System.Text.RegularExpressions;
 
 namespace Calculator_001
 {
     public partial class Form1 : Form
     {
+        decimal firstNumber;
+        decimal secondNumber;
+        decimal totalNumber;
+        string oper;
+
         public Form1()
         {
             InitializeComponent();
@@ -142,42 +148,86 @@ namespace Calculator_001
 
         private void btnEquals_Click(object sender, EventArgs e)
         {
+            secondNumber = decimal.Parse(txtDisplay.Text);
+            switch (oper)
+            {
+                case "+":
+                    {
+                        totalNumber = firstNumber + secondNumber;
+                        break;
+                    }
+                case "-":
+                    {
+                        totalNumber = firstNumber - secondNumber;
+                        break;
+                    }
+                case "*":
+                    {
+                        totalNumber = firstNumber * secondNumber;
+                        break;
+                    }
+                case "/":
+                    {
+                        totalNumber = firstNumber / secondNumber;
+                        break;
+                    }
+                default:
+                    txtDisplay.Text = "";
+                    break;
 
+            }
+            txtDisplay.Text = totalNumber.ToString();
         }
 
         private void btnAddition_Click(object sender, EventArgs e)
         {
-
+            firstNumber = decimal.Parse(txtDisplay.Text);
+            oper = "+";
+            txtDisplay.Text = "0";
         }
 
         private void btnSubtraction_Click(object sender, EventArgs e)
         {
-
+            firstNumber = decimal.Parse(txtDisplay.Text);
+            oper = "-";
+            txtDisplay.Text = "0";
         }
 
         private void btnMultiplication_Click(object sender, EventArgs e)
         {
-
+            firstNumber = decimal.Parse(txtDisplay.Text);
+            oper = "*";
+            txtDisplay.Text = "0";
         }
 
         private void btnDivision_Click(object sender, EventArgs e)
         {
-
+            firstNumber = decimal.Parse(txtDisplay.Text);
+            oper = "/";
+            txtDisplay.Text = "0";
         }
 
         private void btnBackspace_Click(object sender, EventArgs e)
         {
-
+            // need to make the backspace button read the txtDisplay.Text, then subtract the far right char. good luck https://stackoverflow.com/questions/3566830/what-method-in-the-string-class-returns-only-the-first-n-characters
+            strings.Left(txtDisplay.Text, txtDisplay.Text.Length);
         }
+
+        /*Both buttons are a way to erase or clear an entry. 
+         * The CE (clear entry) button clears the most recent entry 
+         * while the C (clear) button will clear all input to the calculator. */
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-
+            txtDisplay.Text = "0";
+            firstNumber = 0;
+            secondNumber = 0;
         }
 
         private void btnClearEntry_Click(object sender, EventArgs e)
         {
-
+            txtDisplay.Text = "0";
+            secondNumber = 0;
         }
 
         private void btnPlusOrMinus_Click(object sender, EventArgs e)
